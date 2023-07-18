91264e392f2f6850eb4f2ede5303effb15d1dea2
Shivangi Garg <48738434+sgarg12@users.noreply.github.com>
2023-07-18 08:27:57 +0000

    formatting

---
+++ mylib/logic.py
@@ -0,0 +1,15 @@
+import wikipedia
+
+
+def wiki(name="War Goddess", length=1):
+    """This is a wikipedia fetcher"""
+
+    my_wiki = wikipedia.summary(name, length)
+    return my_wiki
+
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    results = wikipedia.search(name)
+    return results
